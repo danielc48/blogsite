47c646465093c1af8c45d9a9b431bb6b2d13d9f9
danielc48 <danielschoi48@gmail.com>
2022-07-04 15:39:50 -0700

    Added error handling and data validation
    Created error handler, custom error handler class, client-side form validation, and error page template

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const Article = require('./models/article') ;
 const methodOverride = require('method-override');
 const engine = require('ejs-mate');
-
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressErrors')
 
 main().catch(err => console.log(err));
 
@@ -23,45 +24,57 @@ app.get('/', (req,res) => {
     res.render('home')
 })
 
-app.get('/articles', async(req,res) => {
+app.get('/articles', catchAsync(async(req,res) => {
     const articles = await Article.find({}).sort({date: 'desc'});
     res.render('articles/index', {articles})
-});
+}));
 
 app.get('/articles/new', (req,res) => {
     res.render('articles/new')
 })
 
-app.post('/articles', async (req,res) => {
+app.post('/articles', catchAsync(async (req,res) => {
+    if (!req.body.article) throw new ExpressError(400, 'Invalid Campground Data')
     const article = new Article(req.body.article)
     await article.save();
     res.redirect(`/articles/${article._id}`)
-});
+}));
 
-app.get('/articles/:id', async(req,res) => {
+app.get('/articles/:id', catchAsync(async(req,res,next) => {
     const article = await Article.findById(req.params.id)
     res.render('articles/show', {article})
-})
+}))
 
-app.get('/articles/:id/edit', async(req,res) => {
+app.get('/articles/:id/edit', catchAsync(async(req,res) => {
     const article = await Article.findById(req.params.id);
     res.render('articles/edit', {article})
-})
+}))
 
-app.put('/articles/:id', async(req,res) => {
+app.put('/articles/:id', catchAsync(async(req,res) => {
     const { id } = req.params;
     const article = await Article.findById(id)
     article.title = req.body.article.title;
     article.description = req.body.article.description;
     article.markdown = req.body.article.markdown;
+    article.author = req.body.article.author;
     await article.save()
     res.redirect(`/articles/${article._id}`)
-})
+}))
 
-app.delete('/articles/:id', async(req,res) => {
+app.delete('/articles/:id', catchAsync(async(req,res) => {
     const {id} = req.params;
     await Article.findByIdAndDelete(id);
     res.redirect('/articles')
+}))
+
+app.all('*', (req,res,next) => {
+    next(new ExpressError(404, "Page Not Found"))
+})
+
+app.use((err,req,res,next) => {
+    const {statusCode = 500} = err
+    if (!err.message) err.message = 'Oh no, something went wrong!'
+    res.status(statusCode).render('error', {err})
 })
 
 app.listen(3000, () => {
